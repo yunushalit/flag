6dd13c132604b778abaa3c8752597470ea05380c
yunushalit <kashapov@yandexlyceum.ru>
2019-10-10 23:36:28 +0400

    Серый фон

--- flag.py
+++ flag.py
@@ -6,11 +6,7 @@ def run_game():
     sc = pygame.display.set_mode((400, 450))
     bg_color = (220, 220, 255)
     sc.fill(bg_color)
-    # здесь будут рисоваться фигуры
-    pygame.draw.rect(sc, (156, 55, 0), (50, 20, 10, 375))
-    # белая часть фигуры
-    pygame.draw.rect(sc, (255, 255, 255), (52, 20, 300, 60))
-
+   # Серый фон
     pygame.display.flip()
 
     while True:
